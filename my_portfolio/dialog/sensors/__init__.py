3656f93234087414ed52c242936f70129a79a23a
masahiro116 <66928696+k-masahiro116@users.noreply.github.com>
2023-11-02 13:33:35 +0900

    silent_play削除

--- my_portfolio/dialog/sensors/__init__.py
+++ my_portfolio/dialog/sensors/__init__.py
@@ -1,9 +1,5 @@
 from ..sensors.forecasts import Forecasts
-from ..sensors.silent_play import Stream, surveillance, index_search
 
 __all__ = [
     "Forecasts",
-    "Stream",
-    "surveillance",
-    "index_search",
 ]

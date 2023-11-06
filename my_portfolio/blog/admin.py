3e3c5451811663f7f23f449c84e0cea712a9e5f7
masahiro116 <66928696+k-masahiro116@users.noreply.github.com>
2023-11-06 14:31:36 +0900

    markdown削除

--- my_portfolio/blog/admin.py
+++ my_portfolio/blog/admin.py
@@ -2,7 +2,6 @@ from django import forms
 from django.contrib import admin
 from .models import * # 追加
 from django_summernote.admin import SummernoteModelAdmin
-from markdownx.admin import MarkdownxModelAdmin
 
 class PostCreateForm(forms.ModelForm): 
     summernote_fields = ('text')
@@ -10,5 +9,4 @@ class PostCreateForm(forms.ModelForm):
         model = Post 
         fields = ('title', 'text') 
 admin.site.register(Post, SummernoteModelAdmin)
-# admin.site.register(Post, MarkdownxModelAdmin)
 
